439998f5e1cd010860097c6f17d281c245fdd1e7
sagitush <44786027+sagitush@users.noreply.github.com>
2019-02-09 22:02:07 +0200

    Add files via upload

---
+++ 6.2.19h.w/6.2.19h.w/Citizen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._19h.w
+{
+    class Citizen
+    {
+        public string name;
+        public Citizen[] children;
+        public int fatherId;
+        public readonly int id=1;
+        public static int numberOfCurrentCitizens;
+        public const int maximumNumberOfCitizens=50;
+
+        public Citizen(string name, int fatherId)
+        {
+            this.name = name;
+            this.fatherId = fatherId;
+            this.id++;
+            numberOfCurrentCitizens++;
+        }
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"Number of current citizens:{Citizen.numberOfCurrentCitizens}");
+        }
+        public static bool ReachedHalfOfMaximumSize()
+        {
+            if (numberOfCurrentCitizens >= maximumNumberOfCitizens)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public void PrintId()
+        {
+            Console.WriteLine($"ID number:{this.id}");
+        }
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            int gap = this.id - this.fatherId;
+            Console.WriteLine($"The gap between my ID and my father:{gap}");
+        }
+        public void SetChildren(Citizen[]c)
+        {
+            this.children = new Citizen[c.Length];
+            for (int i = 0; i < c.Length; i++)
+            {
+                this.children[i].name = c[i].name;
+                this.children[i].fatherId = c[i].fatherId;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Citizen name:{this.name},ID:{this.id},father ID:{this.fatherId},number of current citizens:{Citizen.numberOfCurrentCitizens}";
+        }
+    }
+}
---
+++ 6.2.19h.w/6.2.19h.w/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._19h.w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen Noam = new Citizen("Noam", 5);
+            
+            Citizen Bob = new Citizen("Bob", 1);
+            Citizen Alma = new Citizen("Alma", 1);
+            Citizen Shir = new Citizen("Shir", 1);
+            Citizen[] citizens = { Bob, Alma, Shir };
+            Noam.SetChildren(citizens);
+            Console.WriteLine(Noam);
+            Console.WriteLine(HasChildren(Noam));
+            Console.WriteLine(ChekValidity(Noam));
+
+            
+
+        }
+        public static bool HasChildren(Citizen b)
+        {
+            int NumberOfChildren = 0;
+            for (int i = 0; i <b.children.Length; i++)
+            {
+                if(b.children[i].name!=null)
+                {
+                    NumberOfChildren++;
+                }
+            }
+            if (NumberOfChildren==0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool ChekValidity(Citizen b)
+        {
+            int father = 0;
+            if (HasChildren(b) == true)
+            {
+                for (int i = 0; i < b.children.Length; i++)
+                {
+                    if (b.children[i].fatherId == b.id)
+                    {
+                        father++;
+                    }
+                }
+                if (b.children.Length == father)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+                return false;
+        }
+    }
+}
